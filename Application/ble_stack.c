9aa5a67bf1353c698cc8681969ba5121cc6eb9fc
russelmallari <mallari.russelroman@gmail.com>
2023-04-22 20:00:40 +0800

    Added device name

--- Application/ble_stack.c
+++ Application/ble_stack.c
@@ -27,8 +27,8 @@
 
 #define DEVICE_NAME         "x-parasite"
 
-#define APP_ADV_INTERVAL  300
-#define APP_ADV_DURATION  0   // No Timeout
+#define APP_ADV_INTERVAL  3200     // Units of 0.625 ms
+#define APP_ADV_DURATION  0       // No Timeout
 
 BLE_ADVERTISING_DEF(m_advertising);
 
@@ -51,6 +51,14 @@ void ble_stack_init(void)
 
   err_code = nrf_sdh_ble_enable(&ram_start);
   APP_ERROR_CHECK(err_code);
+
+  ble_gap_conn_sec_mode_t sec_mode;
+
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
+
+  err_code = sd_ble_gap_device_name_set(&sec_mode,
+                                          (const uint8_t *)DEVICE_NAME,
+                                          strlen(DEVICE_NAME));
 }
 
 
