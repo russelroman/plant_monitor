388580091581c967e6bf76a9556a3575efdfcf2e
russelmallari <mallari.russelroman@gmail.com>
2023-05-14 10:34:21 +0800

    Refactored temp_hum.c

--- Application/temp_hum.c
+++ Application/temp_hum.c
@@ -16,22 +16,19 @@
 #define TWI_INSTANCE_ID     1
 #endif
 
- /* Number of possible TWI addresses. */
- #define TWI_ADDRESSES      127
-
-#define ARDUINO_SCL_PIN             NRF_GPIO_PIN_MAP(1, 10)    // SCL signal pin
-#define ARDUINO_SDA_PIN             NRF_GPIO_PIN_MAP(1, 13)    // SDA signal pin
+#define SCL_PIN             NRF_GPIO_PIN_MAP(1, 10)    // SCL signal pin
+#define SDA_PIN             NRF_GPIO_PIN_MAP(1, 13)    // SDA signal pin
 
 const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
-uint8_t sample_data[6];
+
 
 void temp_hum_init(void)
 {
     ret_code_t err_code;
 
     const nrf_drv_twi_config_t twi_config = {
-       .scl                = ARDUINO_SCL_PIN,
-       .sda                = ARDUINO_SDA_PIN,
+       .scl                = SCL_PIN,
+       .sda                = SDA_PIN,
        .frequency          = NRF_DRV_TWI_FREQ_100K,
        .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
        .clear_bus_init     = false
@@ -45,6 +42,7 @@ void temp_hum_init(void)
 
 int read_data_shtc(float *temperature, float *humidity)
 {
+  uint8_t sample_data[6];
 
   uint8_t address = 0x70;
   uint8_t wakeup_command[2] = {0x35, 0x17};
@@ -52,7 +50,6 @@ int read_data_shtc(float *temperature, float *humidity)
   uint8_t measure_command[2] = {0x78, 0x66};
   
   NRF_LOG_INFO("Read temp");
- // NRF_LOG_FLUSH();
 
   nrf_drv_twi_tx(&m_twi, address, wakeup_command, sizeof(wakeup_command), false);
   nrf_delay_us(250);  // Wakeup Time based in datasheet.
